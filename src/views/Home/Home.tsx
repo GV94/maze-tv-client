cf9ba8637dcbf9f027e0f14e5d702c9173c067fc
Victor Gustafsson <vgustaf5@volvocars.com>
2023-11-26 15:33:45 +0100

    feat(router): add react-router and create a route
    we break out the functionality that was previously in app and call it the "home page"

---
+++ src/views/Home/Home.tsx
@@ -0,0 +1,66 @@
+import { FC, useEffect, useState } from 'react';
+import { client } from '../../api/client';
+import { GetScheduleResponse } from '../../api/responses/ScheduleListResponse';
+import { SearchResponse } from '../../api/responses/SearchResponse';
+import fallbackImg from '../../assets/thumbnail_fallback.webp';
+import { Listing } from '../../components/Listing';
+import { Search } from '../../components/Search';
+import { getTodaysDate } from '../../utils/date';
+
+export const Home: FC = () => {
+    const [scheduledShows, setScheduledShows] = useState<GetScheduleResponse>(
+        []
+    );
+
+    const [searchResults, setSearchResults] = useState<SearchResponse>([]);
+
+    useEffect(() => {
+        (async () => {
+            const result = await client().getSchedule(getTodaysDate(), 'us');
+            setScheduledShows(result);
+        })();
+    }, []);
+
+    const search = async (query: string) => {
+        const result = await client().search(query);
+        setSearchResults(result);
+    };
+
+    const stripHtmlOfTags = (html: string): string => {
+        return html.replace(/<[^>]*>?/gm, '');
+    };
+
+    return (
+        <div>
+            <h1>Astra TV</h1>
+            <Search onChange={(e) => search(e.target.value)} />
+            <h2>Today's Schedule</h2>
+            <p>{getTodaysDate()}</p>
+            {searchResults && searchResults?.length > 0 ? (
+                <Listing
+                    listItems={searchResults.map(({ show }) => ({
+                        id: show.id,
+                        title: show.name,
+                        summary: stripHtmlOfTags(show.summary ?? '').slice(
+                            0,
+                            100
+                        ),
+                        image: show.image?.original ?? fallbackImg,
+                    }))}
+                />
+            ) : (
+                <Listing
+                    listItems={scheduledShows.map((show) => ({
+                        id: show.id,
+                        title: show.name,
+                        summary: stripHtmlOfTags(show.summary ?? '').slice(
+                            0,
+                            100
+                        ),
+                        image: show.image?.original ?? fallbackImg,
+                    }))}
+                />
+            )}
+        </div>
+    );
+};
