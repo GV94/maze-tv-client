a89f1e69d8c4662995860f47ba9e4b1f616e905e
Victor Gustafsson <vgustaf5@volvocars.com>
2023-11-26 13:08:15 +0100

    feat: add fallback thumbnail image

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import { client } from './api/client';
 import { GetScheduleResponse } from './api/responses/ScheduleListResponse';
+import fallbackImg from './assets/thumbnail_fallback.webp';
 import { Listing } from './components/Listing';
 import { getTodaysDate } from './utils/date';
-
 function App() {
     const [scheduledShows, setScheduledShows] = useState<GetScheduleResponse>(
         []
@@ -31,7 +31,7 @@ function App() {
                     id: show.id,
                     title: show.name,
                     summary: stripHtmlOfTags(show.summary ?? '').slice(0, 100),
-                    image: show.image?.original ?? '',
+                    image: show.image?.original ?? fallbackImg,
                 }))}
             />
         </div>
