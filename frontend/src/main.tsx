ea09fccf9a872cbcb7b6cf2506ed263ba9935388
Victor Gustafsson <vgustaf5@volvocars.com>
2023-11-27 15:50:06 +0100

    refactor: remove App, it serves no purpose

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { clientFactory } from './api/clientFactory.ts';
 import './index.scss';
+import { Home } from './views/Home/index.tsx';
+import { ShowDetails } from './views/ShowDetails/index.tsx';
 
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Home />,
+    },
+    {
+        path: '/show/:id',
+        element: <ShowDetails />,
+        loader: async ({ params }) => {
+            const client = clientFactory();
+            return { show: await client.getShow(Number(params.id)) };
+        },
+    },
+]);
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <App />
+        <RouterProvider router={router} />
     </React.StrictMode>
 );
