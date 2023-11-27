bdafc0629d41fef5d4e3da6a7fe2829da8a65436
Victor Gustafsson <vgustaf5@volvocars.com>
2023-11-27 16:03:46 +0100

    style(Card): format

--- frontend/src/components/Card/index.tsx
+++ frontend/src/components/Card/index.tsx
@@ -1,7 +1,8 @@
 import { FC } from 'react';
+import { cropText } from '../../utils/text';
 import './Card.scss';
 import { CardProps } from './CardProps';
-import { cropText } from '../../utils/text';
+
 export const Card: FC<CardProps> = ({ title, subtitle, summary, image }) => {
     return (
         <div className="card">
@@ -9,7 +10,7 @@ export const Card: FC<CardProps> = ({ title, subtitle, summary, image }) => {
             <div className="content">
                 <h3 className="title">{cropText(title, 40)}</h3>
                 {subtitle && <h5 className="subtitle">{subtitle}</h5>}
-                <p className="summary">{summary || 'Summary missing'} </p>
+                <p className="summary">{summary || 'Summary missing'}</p>
             </div>
         </div>
     );
