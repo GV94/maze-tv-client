45b93656f4e07f84d1b14d42683fec1a0e2039ac
Victor Gustafsson <vgustaf5@volvocars.com>
2023-11-27 13:38:31 +0100

    feat(backend/redis): add redis to cache requests

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import { createClient } from 'redis';
 
 const app = express();
 const port = 3000;
@@ -12,12 +13,39 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
+const redisClient = createClient();
+redisClient.on('error', (err) => console.log('Redis Client Error', err));
+await redisClient.connect();
+
 const getUrl = (path: string) => `${BASE_URL}${path}`;
+
+const callApi = async (path: string) => {
+    const response = await fetch(getUrl(path));
+    return response.json();
+};
+
+const resolveDataFromCache = async (url: string) => {
+    const data = await redisClient.get(url);
+    if (!data) return null;
+    return JSON.parse(data);
+};
+
+const requestIsCached = async (url: string) => {
+    return redisClient.exists(url);
+};
 
 app.get('*', async (req, res) => {
     try {
-        const response = await fetch(getUrl(req.url));
-        res.json(await response.json());
+        if (await requestIsCached(req.url)) {
+            const data = await resolveDataFromCache(req.url);
+            return data
+                ? res.json(data)
+                : res.status(404).json({ message: 'Not found' });
+        }
+
+        const json = await callApi(req.url);
+        await redisClient.set(req.url, JSON.stringify(json));
+        return res.json(json);
     } catch (error) {
         res.status(500).json({
             message: (error as Error)?.message || 'Something went wrong',
