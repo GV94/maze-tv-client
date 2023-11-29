9827253e6e909658291eaf6ec65ea8e9981c40d0
Victor Gustafsson <victor.gustafsson@volvocars.com>
2023-11-29 12:32:41 +0000

    chore: get rid of instanbul sourcemap warning

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 import istanbul from 'vite-plugin-istanbul';
 // https://vitejs.dev/config/
 export default defineConfig({
+    build: { sourcemap: true },
     plugins: [
         react(),
         istanbul({
