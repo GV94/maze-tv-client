fb7fd13068ae2c93115a46c3d57b44bee178d074
Victor Gustafsson <vgustaf5@volvocars.com>
2023-11-29 09:28:17 +0100

    chore: collect coverage from all source files
    previously only files affected by tests had coverage

--- backend/jest.config.ts
+++ backend/jest.config.ts
@@ -1,6 +1,10 @@
 export default {
     preset: 'ts-jest/presets/default-esm',
     testEnvironment: 'node',
+    collectCoverageFrom: [
+        '**/*.ts',
+        '!**/node_modules/**',
+      ],
     transform: {
         '^.+\\.tsx?$': [
             'ts-jest',
